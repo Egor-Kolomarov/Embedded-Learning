85623995818046f61c9cf55b56ebec8819ce4d5a
Egor-Kolomarov <kolomarovegor@gmail.com>
2026-07-08 02:07:45 +0300

    L16: char

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,6 @@
 #include <Arduino.h>
 
-struct Car
-{
-    String brand;
-    int year;
-};
-
-Car car;
-
-void printCar(const Car &car) {
-    Serial.println(car.brand);
-    Serial.println(car.year);
-}
+char word[] = "ESP32";
 
 void setup() {
     Serial.begin(105200);
@@ -19,9 +8,6 @@ void setup() {
 
 
 void loop() {
-    
-    car.brand = "Toyota";
-    car.year = 2020;
-
-    printCar(car);
+    Serial.println(word[0]);
+    Serial.println(word[4]);
 }
