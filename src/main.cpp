391f25c310a893797288156da5ed5bc3d9f93da5
Egor-Kolomarov <kolomarovegor@gmail.com>
2026-07-12 22:11:53 +0300

    L21: 2 LED 1 BUT))))

--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,25 @@
 #include <Arduino.h>
 
 const int buttonPin = 4;
-const int ledPin = 18;
+const int ledPin = 5;
+const int ledPin2 = 18;
+bool previosButtonState = HIGH;
+bool ledState1 = false;
 
-void printBut() {
-    Serial.println(digitalRead(buttonPin));
-    delay(500);
-}
+
 void setup() {
     Serial.begin(115200);
     pinMode(buttonPin, INPUT_PULLUP);
     pinMode(ledPin, OUTPUT);
+    pinMode(ledPin2, OUTPUT);
 }
 
 void loop() {
-    if (digitalRead(buttonPin) == 0) {
-        digitalWrite(ledPin, HIGH);
+    bool currentButtonState = digitalRead(buttonPin);
+    if (previosButtonState == HIGH && currentButtonState == LOW) {
+            ledState1 = !ledState1;
+            digitalWrite(ledPin, ledState1);
+            digitalWrite(ledPin2, !ledState1);
     }
-    else {
-        digitalWrite(ledPin, LOW);
-    }
-    printBut();    
-}
+    previosButtonState = currentButtonState;
+} 
