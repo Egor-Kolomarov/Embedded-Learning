79a35669b29a3f14e83ab95288a36b6bf6ebd98b
Egor-Kolomarov <kolomarovegor@gmail.com>
2026-07-14 03:21:19 +0300

    nighttime lighthouse

--- src/main.cpp
+++ src/main.cpp
@@ -1,25 +1,76 @@
 #include <Arduino.h>
 
-const int buttonPin = 4;
 const int ledPin = 5;
-const int ledPin2 = 18;
-bool previosButtonState = HIGH;
-bool ledState1 = false;
+const int f = 5000;
+const int resolution = 8;
 
+unsigned long previosTime = 0;
+unsigned long previosTime2 = 0;
+
+int dutyCycle = 0;
+int step = 5;
+
+enum State{
+    UP,
+    DOWN,
+    OFF
+};
+
+State currentState = UP;
 
 void setup() {
+
     Serial.begin(115200);
-    pinMode(buttonPin, INPUT_PULLUP);
     pinMode(ledPin, OUTPUT);
-    pinMode(ledPin2, OUTPUT);
+    ledcSetup(0, f, resolution);
+    ledcAttachPin(ledPin, 0);
+    
 }
 
 void loop() {
-    bool currentButtonState = digitalRead(buttonPin);
-    if (previosButtonState == HIGH && currentButtonState == LOW) {
-            ledState1 = !ledState1;
-            digitalWrite(ledPin, ledState1);
-            digitalWrite(ledPin2, !ledState1);
+
+    switch (currentState)
+    {
+
+    case UP:
+
+        if (millis() - previosTime >= 50) {
+            dutyCycle += step;
+
+            ledcWrite(0, dutyCycle);
+            previosTime = millis();
+        }
+        if (dutyCycle >= 255) {
+            currentState = DOWN;
+            previosTime2 = millis();
+        }
+        break;
+
+    case DOWN:
+
+        if (millis() - previosTime >= 50) {
+            dutyCycle -= step;
+
+            ledcWrite(0, dutyCycle);
+            previosTime = millis();
+        }
+        if (dutyCycle <= 0) {
+            currentState = OFF;
+            previosTime2 = millis();
+        }
+        break;
+
+    case OFF:
+
+        ledcWrite(0, 0);
+        if (millis() - previosTime2 >= 2000) {
+            currentState = UP;
+            previosTime2 = millis();
+        }
+        break;
+
+    default:
+        break;
     }
-    previosButtonState = currentButtonState;
+
 } 
