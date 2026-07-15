12e8990a734d3dface7f1d43dd15c7a6af09ae96
Egor-Kolomarov <kolomarovegor@gmail.com>
2026-07-16 01:44:25 +0300

    L22: ADC and potentiometer

--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,14 @@
 #include <Arduino.h>
 
 const int ledPin = 5;
+const int ptPin = 33;
 const int f = 5000;
 const int resolution = 8;
 
-unsigned long previosTime = 0;
-unsigned long previosTime2 = 0;
-
-int dutyCycle = 0;
-int step = 5;
-
-enum State{
-    UP,
-    DOWN,
-    OFF
-};
-
-State currentState = UP;
+int adcToPwm(int a) {
+    int result = a >> 4;
+    return result;
+}
 
 void setup() {
 
@@ -29,48 +21,9 @@ void setup() {
 
 void loop() {
 
-    switch (currentState)
-    {
+    int analogValue = analogRead(ptPin);
+    int value = adcToPwm(analogValue);
 
-    case UP:
-
-        if (millis() - previosTime >= 50) {
-            dutyCycle += step;
-
-            ledcWrite(0, dutyCycle);
-            previosTime = millis();
-        }
-        if (dutyCycle >= 255) {
-            currentState = DOWN;
-            previosTime2 = millis();
-        }
-        break;
-
-    case DOWN:
-
-        if (millis() - previosTime >= 50) {
-            dutyCycle -= step;
-
-            ledcWrite(0, dutyCycle);
-            previosTime = millis();
-        }
-        if (dutyCycle <= 0) {
-            currentState = OFF;
-            previosTime2 = millis();
-        }
-        break;
-
-    case OFF:
-
-        ledcWrite(0, 0);
-        if (millis() - previosTime2 >= 2000) {
-            currentState = UP;
-            previosTime2 = millis();
-        }
-        break;
-
-    default:
-        break;
-    }
-
+    ledcWrite(0, value);
 } 
+ 
