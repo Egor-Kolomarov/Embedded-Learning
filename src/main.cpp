e10216debf252686cc9630a27f0e4d485e59d7c3
Egor-Kolomarov <kolomarovegor@gmail.com>
2026-07-17 02:42:17 +0300

    L24: Preferences

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <Arduino.h>
+#include <Preferences.h>
 
 const int ledPin = 5;
 const int butPin = 4;
@@ -6,6 +7,9 @@ const int butPin = 4;
 bool ledState = false;
 volatile bool buttonPressed = false;
 unsigned long previousTime = 1000;
+int count = 0;
+
+Preferences preferences;
 
 void IRAM_ATTR onButtonPressed() {
     buttonPressed = true;
@@ -14,19 +18,32 @@ void IRAM_ATTR onButtonPressed() {
 void setup() {
 
     Serial.begin(115200);
+
     pinMode(ledPin, OUTPUT);
+    
+    preferences.begin("settings", false);
+    ledState = preferences.getBool("state", false);
+    digitalWrite(ledPin, ledState);
+    count = preferences.getInt("counter", 0);
+
     pinMode(butPin, INPUT_PULLUP);
+
     attachInterrupt(butPin, onButtonPressed, FALLING);
 
 }
 
 void loop() {
+    
     if (buttonPressed) {
         if (millis() - previousTime >= 50) {
             ledState = !ledState;
             digitalWrite(ledPin, ledState);
             buttonPressed = false;
+            preferences.putBool("state", ledState);
+            count++;
+            preferences.putInt("counter", count);
             previousTime = millis();
+            Serial.println(count);
         }
     }   
 } 
